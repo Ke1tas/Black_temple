a18881653e13bfe202c66802c6721eb1e1e9bc8f
Laptop <dreyui4@gmail.com>
2025-05-24 21:03:31 +0400

    added the author object type

--- library-app/src/app/authors/author-edit/author-edit.component.ts
+++ library-app/src/app/authors/author-edit/author-edit.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthorService } from '../author.service';
+import { Author } from '../../shared/models/author';
 
 @Component({
   selector: 'app-author-edit',
   templateUrl: './author-edit.component.html',
   styleUrls: ['./author-edit.component.css']
 })
-export class AuthorEditComponent {
+export class AuthorEditComponent implements OnInit {
+  authorForm: FormGroup;
+  isNewAuthor: boolean = true;
+  countries: string[] = ['Россия', 'США', 'Великобритания', 'Франция', 'Германия'];
 
+  constructor(
+    private fb: FormBuilder,
+    private authorService: AuthorService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.authorForm = this.fb.group({
+      id: [0],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      birthDate: ['', Validators.required],
+      country: ['', Validators.required],
+      biography: ['', [Validators.required, Validators.minLength(20)]],
+      booksWritten: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    if (id && id !== 0) {
+      this.isNewAuthor = false;
+      this.authorService.getAuthor(id).subscribe(author => {
+        this.authorForm.patchValue({
+          ...author,
+          birthDate: this.formatDate(author.birthDate)
+        });
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.authorForm.valid) {
+      const author: Author = {
+        ...this.authorForm.value,
+        birthDate: new Date(this.authorForm.value.birthDate)
+      };
+      
+      this.authorService.saveAuthor(author).subscribe({
+        next: () => this.router.navigate(['/authors']),
+        error: (err) => console.error('Ошибка сохранения:', err)
+      });
+    }
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/authors']);
+  }
+
+  private formatDate(date: Date): string {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = ('0' + (d.getMonth() + 1)).slice(-2);
+    const day = ('0' + d.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  }
 }
--- library-app/src/app/authors/author.service.ts
+++ library-app/src/app/authors/author.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Author } from '../shared/models/author';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
+  private authors: Author[] = [
+    {
+      id: 1,
+      name: 'Лев Толстой',
+      birthDate: new Date(1828, 8, 9),
+      country: 'Россия',
+      biography: 'Классик русской литературы',
+      booksWritten: 30
+    },
+    {
+      id: 2,
+      name: 'Джордж Оруэлл',
+      birthDate: new Date(1903, 5, 25),
+      country: 'Великобритания',
+      biography: 'Английский писатель и публицист',
+      booksWritten: 8
+    }
+  ];
 
-  constructor() { }
+  getAuthors(): Observable<Author[]> {
+    return of(this.authors);
+  }
+
+  getAuthor(id: number): Observable<Author> {
+    const author = this.authors.find(a => a.id === id) || this.createEmptyAuthor();
+    return of({...author});
+  }
+
+  saveAuthor(author: Author): Observable<Author> {
+    if (author.id === 0) {
+      author.id = Math.max(...this.authors.map(a => a.id), 0) + 1;
+      this.authors.push({...author});
+    } else {
+      const index = this.authors.findIndex(a => a.id === author.id);
+      if (index !== -1) {
+        this.authors[index] = {...author};
+      }
+    }
+    return of({...author});
+  }
+
+  deleteAuthor(id: number): Observable<void> {
+    this.authors = this.authors.filter(a => a.id !== id);
+    return of();
+  }
+
+  private createEmptyAuthor(): Author {
+    return {
+      id: 0,
+      name: '',
+      birthDate: new Date(),
+      country: '',
+      biography: '',
+      booksWritten: 0
+    };
+  }
 }
