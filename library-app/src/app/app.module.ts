cd82999e13311118999ce4cc95c6de54cc25295d
AlexChirkova <162548434+AlexChirkova@users.noreply.github.com>
2025-05-24 21:44:19 +0400

    Update app.module.ts

--- library-app/src/app/app.module.ts
+++ library-app/src/app/app.module.ts
@@ -8,16 +8,26 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { BookEditComponent } from './books/book-edit/book-edit.component';
+import { AuthorListComponent } from './authors/author-list/author-list.component';
+import { AuthorEditComponent } from './authors/author-edit/author-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReaderListComponent } from './readers/reader-list/reader-list.component';
+import { ReaderEditComponent } from './readers/reader-edit/reader-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     BookListComponent,
-    BookEditComponent
+    BookEditComponent,
+    AuthorListComponent,
+    AuthorEditComponent,
+    ReaderListComponent,
+    ReaderEditComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
